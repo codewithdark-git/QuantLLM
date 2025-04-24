b6d309af6cfecec79f8f63ea7d658aa3c3b41700
Dark Coder <codewithdark90@gmail.com>
2025-04-24 18:54:52 +0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,8 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'QuantLLM'
 copyright = '2025, QuantLLM Team'
 author = 'QuantLLM Team'
-version = '0.1.0'
-release = '0.1.0'
+version = '1.0.0'
+release = '1.0.0'
 
 # RTD configurations
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
